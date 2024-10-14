e82f28fa4a397e9a43df4204bd6bc24eb19061f1
mcBorgar <borgarfb@gmail.com>
2024-10-14 15:22:36 +0200

    koble til json

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-test = "jippi"
+import paramiko
+import json
+
+# Server details
+server_ip = 'server_ip'
+username = 'your_username'
+password = 'your_password'  # It's better to use SSH keys for security
+
+# Connect to the server
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(server_ip, username=username, password=password)
+
+# Read the JSON file
+sftp = ssh.open_sftp()
+remote_file_path = '/path/to/books.json'
+with sftp.open(remote_file_path, 'r') as file:
+    books = json.load(file)
+
+# Close connections
+sftp.close()
+ssh.close()
+
+# Display the books
+print("Books:", books)
