1a6a16e73be1c4cc21cf1cfa54f8da2b8c02ee13
mcBorgar <borgarfb@gmail.com>
2024-10-24 11:10:50 +0200

    design fungerer men ikke funksjonalitet
    har designet men nå fungerer ikke legg til, skal fikse det

--- appLight.py
+++ appLight.py
@@ -9,7 +9,7 @@ from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
 # Main Application Class
-class MainApp(QMainWindow):
+class MainApp(QMainWindow): 
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Bok Database")
